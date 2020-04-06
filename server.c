c5c4837cfb35ad46c7dde43dd395dd579792f36f
Naoya Ito <naoya.ito@smartdrive.co.jp>
2020-04-06 10:20:30 +0900

    add comments to server.c

--- server.c
+++ server.c
@@ -1,41 +1,78 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-int
-main()
-{
- int sock0;
- struct sockaddr_in addr;
- struct sockaddr_in client;
- int len;
- int sock;
+int main() {
+  int sock0;
+  struct sockaddr_in addr;
+  struct sockaddr_in client;
+  int len;
+  int sock;
+  /*
+    ソケットの作成
+    第一引数: domain, プロトコルファミリー指定 (UNIX, IP, IPV6)
+    第二引数: type, 通信方式の指定
+    第三引数: protocol,
+      プロトコルファミリーの種類ごとに1つのプロトコルのみをサポートする場合は0を指定
+    返り値: socketfd, ファイルディスクリプタ
+  */
+  sock0 = socket(AF_INET, SOCK_STREAM, 0);
 
- /* ソケットの作成 */
- sock0 = socket(AF_INET, SOCK_STREAM, 0);
+  /* アドレスの設定 */
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(12345);
+  addr.sin_addr.s_addr = INADDR_ANY;
 
- /* ソケットの設定 */
- addr.sin_family = AF_INET;
- addr.sin_port = htons(12345);
- addr.sin_addr.s_addr = INADDR_ANY;
- bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+  /*
+    ポート番号などをソケットに割り当てる
+    第一引数: sockfd, ファイルディスクリプタ
+    第二引数: addr, ソケットに割り当てるアドレス
+    第三引数: addrlen, addrの指す構造体のサイズ
+    返り値: 成功した場合は0, エラー時は-1
+  */
+  bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
 
- /* TCPクライアントからの接続要求を待てる状態にする */
- listen(sock0, 5);
+  /*
+    TCPクライアントからの接続要求を待てる状態にする
+    sockfdが参照するソケットを接続待ちソケット(passive socket)として印をつける
+    第一引数: sockfd, ファイルディスクリプタ
+    第二引数: backlog, 保留中のキューのmax,
+      maxの状態で接続要求が到着するとクライアントはエラーを受け取る
+    返り値: 成功した場合は0, エラー時は-1
+  */
+  listen(sock0, 5);
 
- /* TCPクライアントからの接続要求を受け付ける */
- len = sizeof(client);
- sock = accept(sock0, (struct sockaddr *)&client, &len);
+  len = sizeof(client);
+  /*
+    クライアントから通信接続要求が来るまでプログラムを停止し接続後にプログラムを再開する
+    第一引数: sockfd, ファイルディスクリプタ
+    第二引数: addr, 接続した相手のIPアドレスやポート番号などの情報
+    第三引数: addrlen, addrの指す構造体のサイズ
+    返り値: 成功した場合は受け付けたソケットのファイルディスクリプタ,
+    エラー時は-1
+  */
+  sock = accept(sock0, (struct sockaddr *)&client, &len);
 
- /* 5文字送信 */
- write(sock, "HELLO", 5);
+  /*
+    データの送信
+    第一引数: fd, ファイルディスクリプタ
+    第二引数: addr, 接続した相手のIPアドレスやポート番号などの情報
+    第三引数: addrlen, addrの指す構造体のサイズ
+    返り値: 成功した場合はsockfd, エラー時は-1
+  */
+  write(sock, "HELLO", 5);
 
- /* TCPセッションの終了 */
- close(sock);
+  /*
+    ソケットを閉じて通信接続を終了する
+    クローズされたファイルティスクリプタはどのファイルからも参照されていない状態になり再利用できる
+    TCPではcloseの代わりにshutdownも使える
+    第一引数: fd, ファイルディスクリプタ
+    返り値: 成功した場合は0, エラー時は-1
+  */
+  close(sock);
 
- /* listen するsocketの終了 */
- close(sock0);
-
- return 0;
+  /* listen するsocketの終了 */
+  close(sock0);
+  return 0;
 }
